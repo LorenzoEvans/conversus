95b90a5e594ff817d48c3c801e0f2904dbaa5376
LorenzoEvans <lorenzo.evans94@gmail.com>
2020-07-03 16:40:59 -0700

    Making some changes to state while working on basic view

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,44 @@
 use iced::{
     button, scrollable, text_input, Align, Application, Button, Checkbox,
     Column, Command, Container, Element, Font, HorizontalAlignment, Length,
-    Row, Scrollable, Settings, Text, TextInput,
+    Row, Scrollable, Settings, Text, TextInput, Sandbox
 };
-#[derive(Default)]
-pub struct Agora {
-    pub name: String,
-    pub topic: String,
-    pub desc: String,
-    pub conversations: Option<Vec<Conversation>>,
-    pub founder: User,
-
+pub struct Conversus {
+    agoras: Vec<Agora>,
+    users: Vec<User>
 }
+#[derive(Debug, Clone)]
+pub struct Comment(String);
+
+#[derive(Debug, Clone)]
+pub struct Conversation {
+    pub assembly: Vec<User>,
+    pub presenter: User,
+    // reactions to the conversation?
+    pub posts: Vec<Comment>
+}
+
+#[derive(Debug, Clone)]
+struct Panelist(String);
+impl Default for Panelist {
+    fn default() -> Panelist {
+        let panelist = Panelist(String::from("Panelist"));
+        panelist
+    }
+}
+#[derive(Debug, Clone)]
+struct Wayfarer(String);
+impl Default for Wayfarer {
+    fn default() -> Wayfarer {
+        let wayfarer = Wayfarer(String::from("Wayfarer"));
+        wayfarer
+    }
+}
+
+
 #[derive(Debug, Clone, Default)]
 pub struct User {
-    pub kind: UserType,
+    pub kind: String,
     pub user_name: String,
     pub email: String,
     pub password: String,
@@ -23,24 +47,19 @@ pub struct User {
     pub comments: Vec<String>
 
 }
-#[derive(Debug, Clone)]
-pub enum UserType {
-    Panelist,
-    Wayfarer
+
+pub struct Agora {
+    pub name: String,
+    pub topic: String,
+    pub desc: String,
+    pub conversations: Vec<Conversation>,
+    pub founder: User,
+
 }
-#[derive(Debug, Clone)]
-struct Comment(String);
+
 // How will we keep comments in order?
     // incrementing each comment per conversation works, but only if
     // we don't allow threading (or we can treat nested threads like new conversations!)
-#[derive(Debug, Clone)]
-pub struct Conversation {
-    pub assembly: Vec<User>,
-    pub presenter: User,
-    // reactions to the conversation?
-    pub posts: Vec<Comment>
-
-}
 
 #[derive(Debug, Clone)]
 pub enum AgoraMessage {
@@ -55,7 +74,7 @@ pub enum ConversationMessage {
 }
 
 pub enum UserMessage {
-    KindChange(UserType),
+    KindChange(String),
     UserNameChange(String),
     EmailChange(String),
     PasswordChange(String),
@@ -64,7 +83,12 @@ pub enum UserMessage {
     AddedComment(Comment),
     RemovedComment(String)
 }
-
+#[derive(Debug, Clone)]
+pub enum Message {
+    AgoraMessage,
+    UserMessage,
+    ConversationMessage
+}
 impl Agora {
     pub fn new (
         name: String, 
@@ -76,14 +100,19 @@ impl Agora {
                 name,
                 topic,
                 desc,
-                conversations: Some(conversations),
+                conversations: conversations,
                 founder: founder
             }
         }
     pub fn update(&mut self, message: AgoraMessage) {
         match message {
             AgoraMessage::ConversationAdded(conversation) => {
-                self.conversations.unwrap().push(conversation)
+
+                // *self = Conversations::
+                let old_conversations = self.conversations.clone();
+                let old_conversations = old_conversations.push(conversation);
+                let new_conversation = old_conversations;
+                self.conversations = Vec{..new_conversation};
             }
             AgoraMessage::NameChanged(name) => {
                 self.name = name
@@ -92,31 +121,8 @@ impl Agora {
                 self.desc = desc
             }
         }
-    }
+    // }
 }
-
-// impl Default for Agora {
-//     fn default(
-//         name: String, 
-//         topic: String, 
-//         desc: String, 
-//         conversations: Vec<Conversation>, 
-//         founder: User) -> Agora {
-//             let name = String::from("Convo Royale!");
-//             let topic = String::from("Whatever dominates the conversation");
-//             let desc = String::from("May the best topic win.");
-//             let conversation = 
-//             let conversations = Vec::new();
-//             // conversations.push(conversation);
-
-//             Agora {
-//                 name: name, 
-//                 topic: topic,
-//                 desc: desc,
-//             }
-//         }
-// }
-
 
 impl Conversation {
     fn new(assembly: Vec<User>, presenter: User, posts: Vec<Comment>) -> Conversation {
@@ -130,7 +136,7 @@ impl Conversation {
 
 
 impl User {
-    fn new(kind: UserType, user_name: String, password: String, email: String, conversations: Vec<Conversation>, comments: Vec<String>) -> User
+    fn new(kind: String, user_name: String, password: String, email: String, conversations: Vec<Conversation>, comments: Vec<String>) -> User
         {
             User {
                 kind,
@@ -144,6 +150,26 @@ impl User {
 
 }
 
-fn main() {
-    println!("Hello, world!");
+
+// impl Sandbox for Conversus {
+//     type Message = Message;
+
+//     fn new() -> {
+//         Self::default()
+//     }
+//     fn title(&self) -> String {
+//         String::from("Conversus")
+//     }
+
+//     fn update(&mut self, message: Message) {
+//         match message {
+//             Message:: AgoraMessage => match agora_message {
+//                 if let Some(Agoram)
+//             }
+//         }
+//     }
+// }
+
+pub fn main() {
+    Conversus::run(Settings::default())
 }
